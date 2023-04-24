cfe4d592dd4e2832658b699c457eee2a8c7853fd
d-paniutycz <damian.paniutycz@gmail.com>
2023-04-24 13:52:23 +0200

    who are you?

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -4,16 +4,26 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Attribute\AsController;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[AsController]
-class TestController
+readonly class TestController
 {
-    #[Route(path: '/test')]
+    public function __construct(
+        private ParameterBagInterface $parameterBag,
+    ) {
+    }
+
+    #[Route(path: '/who-are-you')]
     public function test(): JsonResponse
     {
-        return new JsonResponse('ok');
+        return new JsonResponse([
+            'app_env' => $this->parameterBag->get('app_env'),
+            'app_name' => $this->parameterBag->get('app_name'),
+            'app_release' => $this->parameterBag->get('app_release'),
+        ]);
     }
 }
